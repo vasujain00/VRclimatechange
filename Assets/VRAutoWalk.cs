e695a39a8b1d9cd7007cefa33dca2bf937535320
vasu jain <vasujain46@gmail.com>
2017-09-25 09:51:29 -0400

    One Commit

---
+++ Assets/VRAutoWalk.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class VRAutoWalk : MonoBehaviour {
+    public float speed = 3.0F;
+    public bool moveForward;
+    private CharacterController controller;
+    private Cardboard gvrViewer;
+    private Transform vrHead;
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+        gvrViewer = transform.GetChild(0).GetComponent<Cardboard>();
+        vrHead = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1"))
+        {
+            moveForward = !moveForward;
+        }
+
+        if(moveForward)
+        {
+            Vector3 forward = vrHead.TransformDirection(Vector3.forward);
+            controller.SimpleMove(forward * speed);
+        }
+	}
+}
